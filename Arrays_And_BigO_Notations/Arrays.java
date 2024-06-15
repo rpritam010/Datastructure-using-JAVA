b75b45eadb8b44321619fce9a428759652da5579
PRITAM RAJ <31183383+rpritam010@users.noreply.github.com>
2024-06-15 15:44:49 +0530

    Array

---
+++ Arrays_And_BigO_Notations/Arrays.java
@@ -0,0 +1,20 @@
+package Arrays_And_BigO_Notations;
+
+public class Arrays {
+    public static void main(String[] args) {
+        //System.out.println("Hello Pritam");
+        int [] intArray = new int [7];
+
+        intArray[0] = 20;
+        intArray[1] = 35;
+        intArray[2] = -15;
+        intArray[3] = 7;
+        intArray[4] = 55;
+        intArray[5] = 1;
+        intArray[6] = -22;
+
+        for (int i:intArray) {
+            System.out.println(i);
+        }
+    }
+}
